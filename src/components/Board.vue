c06d3f104998a34901f0346eeb84fc2fdf43d2dd
lkt <jnssanaee@nate.com>
2019-06-07 14:08:12 +0900

    CLN : Axios 설치
    - Axios 설치 및 일부 소스 적용

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,34 +1,34 @@
 <template>
   <div>
-    Board
-    <div v-if="loading">로딩중....</div>
+    Board 
+    <div v-if="loading">loading board...</div>
     <div v-else>
       <div>bid: {{bid}}</div>
       <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
       <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+      <hr />
+      <router-view></router-view>
     </div>
-    <hr>
-    <router-view></router-view> <!-- 하위 컴포넌트(Card ) 랜더링 영역 -->
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       bid: 0,
       loading: false
     }
   },
-  created(){
+  created() {
     this.fetchData()
   },
   methods: {
-    fetchData(){
+    fetchData() {
       this.loading = true
       setTimeout(() => {
+        this.bid = this.$route.params.bid
         this.loading = false
-        this.bid = this.$route.params.bid // this는 VueComponent 객체
       }, 500)
     }
   }
@@ -36,5 +36,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,55 @@
 <template>
-    <div>Home
-      <div>
-        Board List:
-        <ul>
-          <li>
-            <router-link to="/b/1">Board 1</router-link>
-            <router-link to="/b/2">Board 2</router-link>
-          </li>
-        </ul>
-
+  <div>
+    Home
+    <div>
+      Board List: 
+      <div v-if="loading">Loading...</div>
+      <div v-else>Api result: 
+        <pre>{{apiRes}}</pre>
+        <pre>{{error}}</pre>
       </div>
-
+      <ul>
+        <li>
+          <router-link to="/b/1">Board 1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">Board 2</router-link>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data() {
+    return {
+      loading: false,
+      apiRes: '',
+      error: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      axios.get('http://localhost:3000/health')
+        .then(res => {
+          this.apiRes = res.data
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(_=> {
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
